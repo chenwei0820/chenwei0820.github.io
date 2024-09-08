0bbf57cab581de02c4fd5524095c3631203e8035
wei_c <wei_chne0820@163.com>
2024-09-09 01:28:20 +0800

    test 2

---
+++ src/components/Xigame.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+    <h1>
+      <a href="http://www.baidu.com/">xigame</a>
+    </h1>
+    <hr />
+    <p>游戏获取链接</p>
+    &nbsp;
+    <p>https://pan.baidu.com/s/1EwAnDJ7j2 ni2hKiFr3xBw?pwd=6666 提取码:6666</p>
+    <p>务必收藏网站!防止失联。</p>
+    <p>如果百度网盘失效请用这个网盘直接下载菜单文档!</p>
+    <p>1备用网盘 https://qiwi.gg/folder/6624b743f55f41f6b89c2ab6</p>
+    <p>2备用网盘 https://files.fm/u/tgmn9gcyax</p>
+    <p>客服QQ:3186862737【如有安装不懂的问题可联系客服解决)</p>
+  </div>
+</template>
+  
+  <script>
+</script>
+<style>
+div {
+        margin-left: 10%;
+        margin-right: 10%;
+        /* font-size: 1em; */
+    }
+a {
+  -webkit-text-size-adjust: 100%;
+  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Helvetica, Arial,
+    sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";
+  word-wrap: break-word;
+  font-weight: 600;
+  line-height: 1.25;
+  /* font-size: 1em; */
+  box-sizing: border-box;
+  background-color: transparent;
+  color: #0366d6;
+  text-decoration: none;
+  margin-left: auto;
+}
+/* hr {
+        margin-bottom: 10px;
+        margin-left: auto;
+        margin-right: auto;
+    } */
+p {
+  margin-left: auto;
+  margin-right: auto;
+}
+</style>
+  
