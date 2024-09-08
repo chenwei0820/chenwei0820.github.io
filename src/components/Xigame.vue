2a1ec829a4024055b8f4e11588cac42f3ea386bf
wei_c <wei_chne0820@163.com>
2024-09-09 01:49:29 +0800

    test 2

--- src/components/Xigame.vue
+++ src/components/Xigame.vue
@@ -1,17 +1,17 @@
 <template>
   <div>
     <h1>
-      <a href="http://www.baidu.com/">xigame</a>
+      <a href="https://chenwei0820.github.io/">xigame</a>
     </h1>
     <hr />
     <p>游戏获取链接</p>
     &nbsp;
-    <p>https://pan.baidu.com/s/1EwAnDJ7j2 ni2hKiFr3xBw?pwd=6666 提取码:6666</p>
+    <p>https://pan.baidu.com/s/1EwAnDJ7j2ni2hKiFr3xBw?pwd=6666  提取码:6666</p>
     <p>务必收藏网站!防止失联。</p>
     <p>如果百度网盘失效请用这个网盘直接下载菜单文档!</p>
     <p>1备用网盘 https://qiwi.gg/folder/6624b743f55f41f6b89c2ab6</p>
     <p>2备用网盘 https://files.fm/u/tgmn9gcyax</p>
-    <p>客服QQ:3186862737【如有安装不懂的问题可联系客服解决)</p>
+    <p>客服QQ:3186862737【如有安装不懂的问题可联系客服解决】</p>
   </div>
 </template>
   
@@ -42,9 +42,9 @@ a {
         margin-left: auto;
         margin-right: auto;
     } */
-p {
-  margin-left: auto;
-  margin-right: auto;
-}
+/* p {
+  margin-left: 10%;
+  margin-right: 10%;
+} */
 </style>
   
