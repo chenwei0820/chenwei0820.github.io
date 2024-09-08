20068f4b7646740e1f00243909a36aebc137122d
wei_c <wei_chne0820@163.com>
2024-09-09 01:12:13 +0800

    test 2

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,4 @@
-module.exports = {
-  publicPath : "chenwei0820.github.io",
-  outputDir : "docs"
-}
+const { defineConfig } = require('@vue/cli-service')
+module.exports = defineConfig({
+  transpileDependencies: true
+})
